d21016ce79c8220a66d7544309d9e0dde0da36f5
Raj <rajfekar.143@gmail.com>
2026-05-18 21:25:55 +0530

    update vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    build: {
-      outDir: "dist",
-    },
-  },
-
   tanstackStart: {
     server: {
       preset: "vercel",
